06edb7cc2affb754ab4bfab0416009b1d2d9de12
bluephosphor <66392799+bluephosphor@users.noreply.github.com>
2023-01-20 23:38:27 -0500

    why is good movement so hard

--- src/player.ts
+++ src/player.ts
@@ -1,9 +1,12 @@
 import { Actor, Color, Engine, Input, vec, Vector } from 'excalibur';
 import { Game } from './main';
+import { lerp } from './util';
 
 export class Player extends Actor {
     
     public maxSpeed: number;
+    public accel = 5;
+    public frict = 0.85;
 
     constructor(x: number, y: number) {
         super({
@@ -11,9 +14,10 @@ export class Player extends Actor {
             width: 32,
             height: 32,
             color: Color.Red,
-            pos: vec(x, y)
+            pos: vec(x, y),
         });
         this.maxSpeed = 30;
+        this.motion.inertia = 10;
         Game.add(this);
     }
 
@@ -23,11 +27,16 @@ export class Player extends Actor {
         const controllerX = Number(engine.input.keyboard.isHeld(Input.Keys.ArrowRight)) - Number(engine.input.keyboard.isHeld(Input.Keys.ArrowLeft));
         const controllerY = Number(engine.input.keyboard.isHeld(Input.Keys.ArrowDown))  - Number(engine.input.keyboard.isHeld(Input.Keys.ArrowUp));
 
-        const di = new Vector(controllerX, controllerY).normalize();
+        const di = vec(controllerX, controllerY).normalize();
+        if (controllerX === 0) di.x = 0;
+        if (controllerY === 0) di.y = 0;
 
-        this.vel = new Vector(
-            (controllerX === 0) ?  0 : (this.maxSpeed * delta) * di.x,
-            (controllerY === 0) ?  0 : (this.maxSpeed * delta) * di.y
-        )
+        this.vel.x += (this.accel * delta) * di.x;
+        this.vel.y += (this.accel * delta) * di.y;
+        
+        this.vel.clampMagnitude(this.maxSpeed * delta);
+
+        if (controllerX === 0) this.vel.x *= this.frict;
+        if (controllerY === 0) this.vel.y *= this.frict;
     }
 }
---
+++ src/util.ts
@@ -0,0 +1,8 @@
+export const lerp = (a: number, b: number, amt: number, nosnap?: boolean) => {
+    const _v = a + amt * ( b - a );
+    if (nosnap) {
+        return _v;
+    } else {
+        return Math.abs(_v) > 0.01 ? _v : b;
+    }
+};
